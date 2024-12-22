720c9b939609f345e5eebf0798cbde6e8fd37a0c
Akshay <agarwal.03.akshay@gmail.com>
2024-12-22 11:13:24 -0600

    Initial commit

---
+++ src/test/java/com/akshay/ecomm_webapp/EcommWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.ecomm_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
