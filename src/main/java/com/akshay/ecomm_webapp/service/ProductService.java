86cdddc67d639b43327fc5863ce7e9a362d92bc0
Akshay <agarwal.03.akshay@gmail.com>
2024-12-22 17:46:25 -0600

    Get and Post request implemented

--- src/main/java/com/akshay/ecomm_webapp/service/ProductService.java
+++ src/main/java/com/akshay/ecomm_webapp/service/ProductService.java
@@ -3,15 +3,27 @@ package com.akshay.ecomm_webapp.service;
 import com.akshay.ecomm_webapp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(new Product(101,"Iphone",100000), new Product(102,"Ipad", 50000));
+    List<Product> products = new ArrayList<>(Arrays.asList(new Product(101,"Iphone",100000), new Product(102,"Ipad", 50000)));
 
     public List<Product> getProducts() {
         return products;
+    }
+
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().get();
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
     }
 }
